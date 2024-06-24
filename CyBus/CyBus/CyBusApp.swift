a5a145695ef7632557a60422374309d56be65025
popov.v <m.vadimpopov@gmail.com>
2024-06-24 16:28:05 +0300

    feat(setup): create project

---
+++ CyBus/CyBus/CyBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyBusApp.swift
+//  CyBus
+//
+//  Created by Vadim Popov on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CyBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
