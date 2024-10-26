0d63a65c75c68053664d1e8d5926797bf07413d6
SeredaArt <sereda.russia@gmail.com>
2024-10-26 21:02:15 +0200

    feat(MVP)-welcome-screen

--- CyBus/Features/Onboarding/Presentation/Steps/OnboardingWelcomeView.swift
+++ CyBus/Features/Onboarding/Presentation/Steps/OnboardingWelcomeView.swift
@@ -8,12 +8,65 @@
 import SwiftUI
 import ComposableArchitecture
 
+
+
 struct OnboardingWelcomeView: View {
+    @Environment(\.theme) var theme
     let store: StoreOf<OnboardingFeature>
-    
+  
     var body: some View {
-        PrimaryButton(label: "Get Start") {
-            store.send(.getStartTapped)
+        
+        let backgroundGradient = LinearGradient(
+            colors: [theme.colors.foreground, theme.colors.background],
+            startPoint: .top, endPoint: .bottom)
+
+        ZStack {
+            backgroundGradient
+                .ignoresSafeArea()
+                .safeAreaInset(edge: .top, spacing: 0) {
+                    VStack {
+                        
+                let height = UIScreen.main.bounds.height;
+                let width = UIScreen.main.bounds.width;
+        
+                Text("Welcome to \nCyprusGo!")
+                    .padding(.leading, width*0.0909)
+                    .padding(.bottom, height*0.0209)
+                    .padding(.top, UIScreen.main.bounds.height*0.0262)
+                    .font(theme.typography.largeTitle)
+                    .fontWeight(.bold)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(minWidth: width, alignment: .leading)
+        
+                Text("Track buses in real time, plan \nroutes and add your favorite \nstops to your favorites.")
+                    .padding(.leading, width*0.0909)
+                    .padding(.bottom, height*0.0262)
+                    .font(theme.typography.title)
+                    .fontWeight(.medium)
+                    .foregroundStyle(theme.colors.primary)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: width, alignment: .leading)
+                  
+                Image("onboard_welcome")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: width*0.9791, height: height*0.4874)
+                        .padding(.bottom, height*0.0418)
+               
+                    .safeAreaInset(edge: .bottom, spacing: 0) {
+                        PrimaryButton(label: "Get Start") {
+                            store.send(.getStartTapped)
+                        }  .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                            .padding(.leading, height*0.0628)
+                    }
+                }
+         
+            }
+
         }
+        
     }
+        
 }
+
+
