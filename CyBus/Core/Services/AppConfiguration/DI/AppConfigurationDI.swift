70d79c494d2203f24e668310c5c8a8f4d0ce7475
popov.v <m.vadimpopov@gmail.com>
2025-08-25 20:22:30 -0500

    feat: ads configuration

--- CyBus/Core/Services/AppConfiguration/DI/AppConfigurationDI.swift
+++ CyBus/Core/Services/AppConfiguration/DI/AppConfigurationDI.swift
@@ -9,12 +9,20 @@ import FactoryKit
 import Foundation
 
 struct AppConfiguration {
+    // Base URL of the backend API used for network requests.
     let backendURL: URL
-    let language: String
+    // AdMob unit ID for banner advertisements.
+    let bannerAdUnitID: String
+    // AdMob unit ID for interstitial video advertisements.
+    let interstitialAdUnitID: String
 }
 
 extension Container {
     var appConfiguration: Factory<AppConfiguration> {
-        self { AppConfiguration(backendURL: URL(string: "https://api.cybusapp.org/")!, language: "en") }
+        self { AppConfiguration(
+            backendURL: URL(string: "https://api.cybusapp.org/")!,
+            bannerAdUnitID: "ca-app-pub-3940256099942544/6300978111",
+            interstitialAdUnitID: "ca-app-pub-3940256099942544/8691691433")
+        }
     }
 }
